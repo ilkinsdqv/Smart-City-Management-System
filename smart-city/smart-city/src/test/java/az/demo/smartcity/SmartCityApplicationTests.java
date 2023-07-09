b84aae0269068e97b878decfabb65d342c579229
İlkin <97682974+ilkinsdqv@users.noreply.github.com>
2023-07-09 16:34:19 +0400

    Project Created

---
+++ smart-city/smart-city/src/test/java/az/demo/smartcity/SmartCityApplicationTests.java
@@ -0,0 +1,13 @@
+package az.demo.smartcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
